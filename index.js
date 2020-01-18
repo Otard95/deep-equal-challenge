a86425be82eb16bdebb1d0988fbb1b3b21fcdd7c
Otard95 <stian.myklebostad@getmail.no>
2020-01-18 09:25:06 +0100

    Added asserts for basic functionality'

--- index.js
+++ index.js
@@ -15,10 +15,33 @@ const deepEquals = (a, b) => {
   }
 }
 
-assert.equal(deepEquals( 'a', 'a' ), true, `deepEquals('a', 'a') failed assertion. Expected 'true' but got '${deepEquals('a', 'a')}'`)
-assert.equal(deepEquals(   1, 1   ), true, `deepEquals(1, 1) failed assertion. Expected 'true' but got '${deepEquals(1, 1)}'`)
+// Not tricked by types
+assert.equal(deepEquals( 'a', 'a' ),  true, `deepEquals('a', 'a') failed assertion. Expected 'true' but got '${deepEquals('a', 'a')}'`)
+assert.equal(deepEquals(   1, 1   ),  true, `deepEquals(1, 1) failed assertion. Expected 'true' but got '${deepEquals(1, 1)}'`)
 assert.equal(deepEquals( 'a', 1   ), false, `deepEquals('a', 1) failed assertion. Expected 'false' but got '${deepEquals('a', 1)}'`)
-assert.equal(deepEquals(  {}, {}  ), true, `deepEquals({}, {}) failed assertion. Expected 'true' but got '${deepEquals({}, {})}'`)
+assert.equal(deepEquals(  {}, {}  ),  true, `deepEquals({}, {}) failed assertion. Expected 'true' but got '${deepEquals({}, {})}'`)
 assert.equal(deepEquals(  {}, []  ), false, `deepEquals({}, []) failed assertion. Expected 'false' but got '${deepEquals({}, [])}'`)
 assert.equal(deepEquals(  {}, null), false, `deepEquals({}, null) failed assertion. Expected 'false' but got '${deepEquals({}, null)}'`)
 assert.equal(deepEquals(/a/g, {}  ), false, `deepEquals(/a/g, {}) failed assertion. Expected 'false' but got '${deepEquals(/a/g, {})}'`)
+
+// Works as intended
+assert.equal(deepEquals(
+  { a: 1, b: 'qwe', c: { d: [ 1, 'asd' ] } },
+  { a: 1, b: 'qwe', c: { d: [ 1, 'asd' ] } }
+), true, `deepEquals(...) failed assertion. Expected 'true' but got 'false`)
+assert.equal(deepEquals(
+  { a: 2, b: 'qwe', c: { d: [ 1, 'asd' ] } },
+  { a: 1, b: 'qwe', c: { d: [ 1, 'asd' ] } }
+), false, `deepEquals(...) failed assertion. Expected 'false' but got 'true'`)
+assert.equal(deepEquals(
+  { a: 1, b: 'qwe', c: { d: [ 1, 'asd' ] } },
+  { a: 1, b: 'qwe', c: { d: [ '1', 'asd' ] } }
+), false, `deepEquals(...) failed assertion. Expected 'false' but got 'true'`)
+assert.equal(deepEquals(
+  { a: 1, b: 'qwe', c: { d: [ 1, 'asd' ] } },
+  { a: 1, b: 'qwe', c: { d: [ '1', 'asd' ] } }
+), false, `deepEquals(...) failed assertion. Expected 'false' but got 'true'`)
+assert.equal(deepEquals(
+  [ 1, 'asd', { a: 'b', c: 123 } ]
+  [ 1, 'asd', { a: 'b', c: 123 } ]
+), true, `deepEquals(...) failed assertion. Expected 'true' but got 'false'`)
